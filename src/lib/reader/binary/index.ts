04166c82db7247cd515128defd9b41e6c2891f10
Cristian Abrante <cristian@abrante.me>
2019-04-03 18:36:22 +0100

    Ref: Binary reader index added

---
+++ src/lib/reader/binary/index.ts
@@ -0,0 +1,7 @@
+/*
+ * @license
+ * Copyright (c) 2019 Cristian Abrante. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ */
+
+export { default as BinaryReader } from './BinaryReader';
