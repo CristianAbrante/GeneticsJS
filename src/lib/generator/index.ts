9719b55e5223188eee48433e14bc47579ebaed8c
Cristian Abrante <cristian@abrante.me>
2019-04-09 22:10:58 +0100

    Ref: index updated

---
+++ src/lib/generator/index.ts
@@ -0,0 +1,17 @@
+/*
+ * @license
+ * Copyright (c) 2019 Cristian Abrante. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ */
+
+import * as Base from './base';
+import * as Binary from './binary';
+import * as Numeric from './numeric';
+
+const generator = {
+  ...Base,
+  ...Binary,
+  ...Numeric.default,
+};
+
+export default generator;
