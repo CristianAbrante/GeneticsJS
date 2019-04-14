b4fcbb2b806f6ac199ee73202e160b03c7957a43
Cristian Abrante <cristian@abrante.me>
2019-04-14 23:25:12 +0100

    Ref & Doc: Floating individual creation refactored and documentation added

--- src/lib/individual/numeric/floating/FloatingIndividual.ts
+++ src/lib/individual/numeric/floating/FloatingIndividual.ts
@@ -7,10 +7,36 @@
 import { FloatingReader } from '../../../reader/numeric/floating';
 import { NumericIndividual, NumericRange } from '../base';
 
+/**
+ * ## Floating Individual
+ * Floating individual is an individual which
+ * contains an array of floating point numbers.
+ * The representation could be a string with
+ * the numbers or the array of numbers.
+ */
 class FloatingIndividual extends NumericIndividual {
+  /**
+   * Constructor of the class.
+   * It takes a representation and a range as
+   * parameters.
+   *
+   * Representation could be a string or an
+   * array of numbers. If representation is
+   * a `string`, it takes the default range as
+   * parameter. if it is a number[]` it takes the
+   * specified range.
+   * @param representation
+   * @param range of the individual. Only if representation
+   *        is a `number[]`.
+   * @throws Error if range is not valid.
+   * @throws RangeError if representation is a `number[]` and
+   *          is not in range specified by the range parameter.
+   * @throws Error if representation is an `string` with an
+   *          wrong format.
+   */
   constructor(representation: number[] | string, range?: NumericRange) {
     if (typeof representation === 'string') {
-      super([], range);
+      super([]);
       const reader = new FloatingReader();
       this.copy(reader.read(representation));
     } else {
