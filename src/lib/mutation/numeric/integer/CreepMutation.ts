4e97a44ea9b8bd56bfd1e8680a9020632a99e543
Cristian Abrante <cristian@abrante.me>
2019-04-21 13:04:11 +0100

    Add: non uniform numeric mutations refactored

--- src/lib/mutation/numeric/integer/CreepMutation.ts
+++ src/lib/mutation/numeric/integer/CreepMutation.ts
@@ -5,33 +5,12 @@
  */
 
 import { Generator } from '../../../generator/utils';
-import { NumericRange } from '../../../individual/numeric/base';
 import { IntegerIndividual } from '../../../individual/numeric/integer';
-import Mutation, { MutationParams } from '../../base/Mutation';
+import { NumericNonUniformMutation, NumericNonUniformMutationParams } from '../base';
 
-export interface CreepMutationParams extends MutationParams {
-  stepSize: number;
-}
-
-class CreepMutation implements Mutation<IntegerIndividual, number, CreepMutationParams> {
-  public mutate(individual: IntegerIndividual, stepSize: number = 1, engine = Generator.DEFAULT_ENGINE): void {
-    this.mutateWith(individual, {
-      engine,
-      stepSize,
-    });
-  }
-
-  public mutateWith(individual: IntegerIndividual, params: CreepMutationParams): void {
-    individual.forEach((_, index) => {
-      const gene = this.getMutatedGeneValue(individual, index!, params);
-      individual.set(index!, gene);
-    });
-  }
-
-  private getMutatedGeneValue(individual: IntegerIndividual, index: number, params: CreepMutationParams) {
-    const gene = individual.get(index);
-    const delta = Generator.generateNormalDistributionInteger(0, params.stepSize, params.engine);
-    return NumericRange.normalizeValueToRange(gene + delta, individual.range);
+class CreepMutation extends NumericNonUniformMutation<IntegerIndividual> {
+  protected getDeltaValue(params: NumericNonUniformMutationParams): number {
+    return Generator.generateNormalDistributionInteger(0, params.stepSize, params.engine);
   }
 }
 
