57625191346bf3cea2cf8835eab26c52e02ed1fa
Cristian Abrante <cristian@abrante.me>
2019-05-12 18:54:13 +0100

    Ref: cross return method changed

--- src/lib/crossover/base/Crossover.ts
+++ src/lib/crossover/base/Crossover.ts
@@ -15,7 +15,7 @@ export interface CrossoverParams<I extends BaseIndividual<T>, T> {
 }
 
 interface Crossover<I extends BaseIndividual<T>, T, Params extends CrossoverParams<I, T>> {
-  cross(firstParent: I, secondParent: I, ...args: any[]): I[];
+  cross(firstParent: I, secondParent: I, ...args: any[]): I | I[];
   crossWith(firstParent: I, secondParent: I, params: Params): I | I[];
 }
 
