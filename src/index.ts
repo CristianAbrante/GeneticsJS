906f2394064380e9b0a062daea8e3cbffa5866af
Cristian Abrante <cristian@abrante.me>
2019-05-14 19:55:11 +0100

    Ref: main index refactored

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,8 @@
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  */
 
-import * as generator from './lib/generator';
-import * as individual from './lib/individual';
-import * as mutation from './lib/mutation';
-import * as reader from './lib/reader';
-
-export { default as Mutation, MutationParams } from './lib/mutation/base/Mutation';
-
-const Genetics = {
-  generator: generator.default,
-  individual: individual.default,
-  mutation: mutation.default,
-  reader: reader.default,
-};
-
-export default Genetics;
+export * from './lib/crossover';
+export * from './lib/generator';
+export * from './lib/individual';
+export * from './lib/mutation';
+export * from './lib/reader';
